8355e9170d10b4c9d56229a9141a42721fa5580c
Salih Gumus <sudanmerinosu@gmail.com>
2021-10-30 19:57:40 +0300

    Basic commands added

--- src/client/index.js
+++ src/client/index.js
@@ -1,21 +1,10 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-require('dotenv').config();
+const client = new Client({ intents: 32767 });
 
 const Connect_Client = () => {
 	client.on('ready', () => {
-		console.log(`Logged in as ${client.user.tag}!`);
+		console.log(`Logged in as ${client.user.tag} 🤖!`);
 	});
-
-	client.on('interactionCreate', async (interaction) => {
-		if (!interaction.isCommand()) return;
-
-		if (interaction.commandName === 'ping') {
-			await interaction.reply('Pong!');
-		}
-	});
-
-	client.login(process.env.TOKEN);
 
 	return client;
 };
