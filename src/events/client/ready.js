38a2bd6fbf94e205d1208b55340bac03eebac78d
Salih Gumus <sudanmerinosu@gmail.com>
2021-10-31 12:42:56 +0300

    .env.example changed

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,20 @@
+const { Client } = require('discord.js');
+
+/**
+ * @param {Client} client
+ */
+
+const activitiy = process.env.ACTIVITIY;
+
 module.exports = {
 	name: 'ready',
 	once: true,
+	/**
+	 * @param {Client} client
+	 */
+
 	execute(client) {
 		console.log(`I'm ready to go ${client.user.tag} 🤖!`);
+		client.user.setActivity(activitiy, { type: 'WATCHING' });
 	},
 };
